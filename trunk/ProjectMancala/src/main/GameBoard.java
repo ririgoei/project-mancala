73307ec5e90e61bc48d2033a9cba582327d21259
thossnider@gmail.com <thossnider@gmail.com@e34b5786-ce2e-3a21-cd40-98c3eca6953e>
2011-02-07 21:12:56 +0000

    Added the classes we'll be using, as well as TODO's and explanations on each class. Working on the Framework.

---
+++ trunk/ProjectMancala/src/main/GameBoard.java
@@ -0,0 +1,15 @@
+package main;
+
+/*TODO:
+ * 
+ * In this class, we should build the gameboard and all of it's properties. So things we should include in here are:
+ * 
+ * Each player's pots, with player ownership (although we can do this in the LinkList/Array classes.
+ * We should also have win conditions, pot placement in the window, and pool placement.
+ * Possibly we should have the score and what not placed in here too referenced from the LinkList/Array classes.
+ * 
+ */
+
+public class GameBoard {
+
+}

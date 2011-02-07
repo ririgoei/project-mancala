01d9eb85570e4645a9a9562c371531636186427b
thossnider <thossnider@gmail.com>
2011-02-07 21:12:56 +0000

    Added the classes we'll be using, as well as TODO's and explanations on each class. Working on the Framework.

---
+++ ProjectMancala/src/main/LinkList.java
@@ -0,0 +1,16 @@
+package main;
+
+/*
+ * TODO:
+ * 
+ * In this class, we need to have the LinkList side of the logic. That is if we're using the Link List.
+ * We should have the pot/pool content and logic in this area. Unless we decide to use the Array Method.
+ * For now I'm going to assume we're using the Link List method, which I'll be using reference material from here:
+ * http://www.java-tips.org/java-se-tips/java.lang/linked-list-implementation-in-java.html
+ * 
+ * Hopefully that'll help both of us better to understand Link Lists in Java.
+ */
+
+public class LinkList {
+
+}
---
+++ ProjectMancala/src/main/gameMain.java
@@ -0,0 +1,21 @@
+package main;
+
+/*
+ * This is the gameMain, where we'll do our game loop and all of our execution using the other classes.
+ * 
+ * We've currently got these classes:
+ * 
+ * ArrayMethod: For utilizing Arrays for game logic.
+ * LinkList: For utilizing Link lists for game logic.
+ * GameBoard: Where we'll be doing things involving the game board (possibly drawing it).
+ * 
+ * I'm thinking I'll create a Window/JavaApp class to possibly build the window where we'll be hosting our game.
+ * 
+ */
+
+
+public class gameMain 
+{
+	
+	
+}
